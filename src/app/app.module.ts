2444c0fcfb3bc8b5f4f5f5a76ff3be3784925b96
john3245 <52746923+john3245@users.noreply.github.com>
2020-03-06 07:09:22 -0600

    Senoooor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './inicio/home/home.component';
+import { RegistroComponent } from './registro/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
